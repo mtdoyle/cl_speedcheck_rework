bf817a8a8ab39b8e455524461eda2817227ea7bf
Mike Doyle <mtdoyle@gmail.com>
2015-10-09 14:40:57 -0500

    bash script to download some of the prereqs

---
+++ setup_clspeed.sh
@@ -0,0 +1,6 @@
+wget https://bootstrap.pypa.io/get-pip.py
+python get-pip.py
+pip install pika
+pip install selenium
+apt-get install build-essential python-dev mysql-client libmysqlclient-dev
+pip install mysql-python
